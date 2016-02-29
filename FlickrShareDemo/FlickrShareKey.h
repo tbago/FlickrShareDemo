058dc0d90292e9e17ca435a56359cfbc0c14227d
tbago <tbago@YUNEEC-iMAC.local>
2016-02-29 16:55:10 +0800

    add pod dependency,add flickr key and change appDelegate

---
+++ FlickrShareDemo/FlickrShareKey.h
@@ -0,0 +1,13 @@
+//
+//  FlickrShareKey.h
+//  FlickrShareDemo
+//
+//  Created by tbago on 16/2/29.
+//  Copyright © 2016年 tbago. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FlickrShareKey : NSObject
+
+@end
---
+++ FlickrShareDemo/FlickrShareKey.m
@@ -0,0 +1,13 @@
+//
+//  FlickrShareKey.m
+//  FlickrShareDemo
+//
+//  Created by tbago on 16/2/29.
+//  Copyright © 2016年 tbago. All rights reserved.
+//
+
+#import "FlickrShareKey.h"
+
+@implementation FlickrShareKey
+
+@end
