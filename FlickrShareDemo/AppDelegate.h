03923b44c099550f33efae553ee8e8576bd6ddbf
tbago <tbago@YUNEEC-iMAC.local>
2016-02-29 16:43:43 +0800

    init demo project

---
+++ FlickrShareDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FlickrShareDemo
+//
+//  Created by tbago on 16/2/29.
+//  Copyright © 2016年 tbago. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
